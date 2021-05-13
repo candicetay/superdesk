2d8f9fff97b407c48b106dd79c4b2c275e3345e3
Mark Pittaway <mark.pittaway@gmail.com>
2021-05-13 17:35:35 +1000

    Revert "Update extensions registration"
    This reverts commit 01389717931960952a10008be75ed24db1cf66dd.

--- client/index.js
+++ client/index.js
@@ -1,16 +1,13 @@
 import {startApp} from 'superdesk-core/scripts/index';
+import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
+import datetimeFieldExtension from 'superdesk-core/scripts/extensions/datetimeField/dist/src/extension';
+import planningExtension from 'superdesk-planning/client/planning-extension/dist/extension';
 
 setTimeout(() => {
     startApp(
-        [{
-            id: 'planning',
-            load: () => (
-                import('superdesk-planning/client/planning-extension/dist/extension')
-                    .then((res) => res.default)
-            ),
-        }],
-        {}
-    )
+        [annotationsLibraryExtension, datetimeFieldExtension, planningExtension],
+        {},
+    );
 });
 
 export default angular.module('main.superdesk', []);
